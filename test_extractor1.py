bbb3b19d51efeb3ea4668fc61f1465ea0c1ed5de
Dongsun Kim <darkrsw@gmail.com>
2023-11-07 10:21:04 +0900

    Create test_extractor1.py

---
+++ test_extractor1.py
@@ -0,0 +1,18 @@
+from design_extractor import collectProgramDesign
+
+test_input_path = "./input.py"
+
+def test_app_analyzer():
+    expected = {"MyClassA":
+                 {
+                   "func1": {"num_total_if": 5, "max_nested_if": 5, "num_total_for": 0, "max_nested_for": 0},
+                   "func2": {"num_total_if": 1, "max_nested_if": 1, "num_total_for": 2, "max_nested_for": 2}
+                 },
+                "MyClassB":
+                 {
+                   "func1": {"num_total_if": 2, "max_nested_if": 1, "num_total_for": 2, "max_nested_for": 2}
+                 }
+               }
+
+    # print(collectProgramDesign(test_input_path))
+    assert expected == collect_function_structure(test_input_path)
