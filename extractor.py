495c5c17229336ef020c54475e0773a952451fcc
faysalahmedbhuiyan <147671727+faysalahmedbhuiyan@users.noreply.github.com>
2023-11-07 16:10:28 +0900

    Update extractor 2022110429.py

--- extractor.py
+++ extractor.py
@@ -1,4 +1,40 @@
 import ast
 
+  #{'class1': {'func1': {'num_total_if': num1, 'max_nested_if':
+  # num2, 'num_total_for': num3, 'max_nested_for': num4}, ...}, ...},
 def collect_function_structure(path: str):
+  class Functionvisitor(ast.NodeVisitor):
+    def __func1__(salf):
+            salf.current_function = None
+            salf.current_if_count = 0
+            salf.max_nested_if = 2
+            salf.current_for_count = 0
+            salf.max_nested_for = 4
+
+        def visit_FunctionDef(salf, node):
+            # Start a new function,
+            salf.current_function = node.name
+            salf.current_if_count = 0
+            salf.max_nested_if = 2
+            salf.current_for_count = 0
+            salf.max_nested_for = 4
+            salf.generic_visit(node)
+            # Store all the information in the dictionary
+            function_structure[salf.current_function] = {
+                "num_total_if": salf.current_if_count,
+                "max_nested_if": salf.max_nested_if,
+                "num_total_for": salf.current_for_count,
+                "max_nested_for": salf.max_nested_for,
+            }
+            salf.current_function = None
+
+        def visit_If(self, node):
+            salf.current_if_count = salf.current_if_count+ 1
+            salf.max_nested_if = max(salf.max_nested_if, len(node.orelse))
+            salf.generic_visit(node)
+
+        def visit_For(salf, node):
+            salf.current_for_count = self.current_for_count+ 1
+            salf.max_nested_for = max(self.max_nested_for, 0)  
+            salf.generic_visit(node)
   return {}
